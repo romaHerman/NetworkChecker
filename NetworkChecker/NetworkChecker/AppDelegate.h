a7aef7d88cd15de07a1a22d4119ac70b309804a1
roma <vkgrembox@gmail.com>
2014-12-06 09:33:50 +0200

    AFnetworking added, Realm added

---
+++ NetworkChecker/NetworkChecker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NetworkChecker
+//
+//  Created by Roma Herman on 12/6/14.
+//  Copyright (c) 2014 Roma Herman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
